44665525d1321e95d7042134df017f5a75d9eadd
ricardpaschoeto <ricardopaschoeto@gmail.com>
2026-07-27 18:59:56 -0300

    Versão 2 do projeto

--- treinamento/dsa_treina_modelo.py
+++ treinamento/dsa_treina_modelo.py
@@ -35,7 +35,7 @@ X = pd.DataFrame({
 y = qualidade
 
 # Dividir em treino e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
 # Criar e treinar o modelo
 model = RandomForestClassifier(random_state=42)
